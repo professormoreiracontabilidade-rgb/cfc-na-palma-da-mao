d0ca3f9e3f5b0592f0135246dc026f840c387896
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-07-25 12:13:11 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,27 @@
-```js
-const CACHE_NAME = "cfc-na-palma-v7";
-
+const CACHE_NAME = "cfc-na-palma-v10";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./contabilidade-geral.html",
   "./manifest.json",
-
   "./auditoria.png",
   "./pericia_contabil.png",
   "./contabilidade_publica.png",
   "./contabilidade_de_custos.png",
-  "./ética_contábil.png",
+  "./etica_contabil.png",
   "./contabilidade_geral.png",
-  "./estatística.png",
   "./direito.png",
-  "./matematica_financeira.png",
   "./icon-192.png",
   "./icon-512.png"
 ];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .catch(error => console.log("Erro ao salvar cache:", error))
-  );
-
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
-```
