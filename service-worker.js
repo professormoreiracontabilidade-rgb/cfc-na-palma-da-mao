e69f9d8a0a4a5d9b2e8815b8845af6d09a7d8466
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-07-25 15:17:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "cfc-na-palma-v10";
+const CACHE_NAME = "cfc-na-palma-v11";
+
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -14,14 +15,52 @@ const FILES_TO_CACHE = [
   "./icon-192.png",
   "./icon-512.png"
 ];
+
+// Instala o novo cache
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
   self.skipWaiting();
 });
+
+// Apaga caches antigos
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
   self.clients.claim();
 });
+
+// Busca primeiro a versão mais nova na internet
 self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        const responseClone = networkResponse.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return networkResponse;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
