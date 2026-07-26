4506cfa50af4f3a2b7d838f1d36d2b5e4f509956
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-07-26 13:48:28 -0300

    Adiciona firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+// Importa o Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA-wspl20od5LstQ8oPHVwddw7_W2mjHn8",
+  authDomain: "cfc-na-palma-da-mao.firebaseapp.com",
+  projectId: "cfc-na-palma-da-mao",
+  storageBucket: "cfc-na-palma-da-mao.firebasestorage.app",
+  messagingSenderId: "45671139736",
+  appId: "1:45671139736:web:c2d04ce67aa1425ff43ed",
+  measurementId: "G-J9TJBQTZEC"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa a autenticação
+const auth = getAuth(app);
+
+// Exporta para os outros arquivos
+export { auth };
