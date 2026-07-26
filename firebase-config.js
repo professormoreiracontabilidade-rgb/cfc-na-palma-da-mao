666f7912622238bd261f43ed64d4440015df8998
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-07-26 20:08:29 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 
 // Configuração pública copiada do aplicativo Web no Firebase Console.
 window.CFC_FIREBASE_CONFIG = Object.freeze({
-  apiKey: 'AIzaSyA-wspl20od5LstQ8oPHVwddw7_W2mjHn8',
+  apiKey: "AIzaSyA-wspL20od5LstQ8oPHVwdd7_W2mjHn8",
   authDomain: 'cfc-na-palma-da-mao.firebaseapp.com',
   projectId: 'cfc-na-palma-da-mao',
   storageBucket: 'cfc-na-palma-da-mao.firebasestorage.app',
