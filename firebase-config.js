b7c235fc08ba949bc420bb766efb44715b361ab5
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-07-26 21:35:59 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,14 @@
-'use strict';
+const firebaseConfig = {
+  apiKey: "AIzaSyA-wspl2Ood5LstQ8oPHVwddw7_W2mjHn8",
+  authDomain: "cfc-na-palma-da-mao.firebaseapp.com",
+  projectId: "cfc-na-palma-da-mao",
+  storageBucket: "cfc-na-palma-da-mao.firebasestorage.app",
+  messagingSenderId: "45671139736",
+  appId: "1:45671139736:web:f16803f6627c5c00ff43ed",
+  measurementId: "G-VE3H371RKN"
+};
 
-// Configuração pública copiada do aplicativo Web no Firebase Console.
-window.CFC_FIREBASE_CONFIG = Object.freeze({
-  apiKey: "AIzaSyA-wspL20od5LstQ8oPHVwdd7_W2mjHn8",
-  authDomain: 'cfc-na-palma-da-mao.firebaseapp.com',
-  projectId: 'cfc-na-palma-da-mao',
-  storageBucket: 'cfc-na-palma-da-mao.firebasestorage.app',
-  messagingSenderId: '45671139736',
-  appId: '1:45671139736:web:c2d0d4ce67aa1425ff43ed',
-  measurementId: 'G-J9TJBQTZEC'
-});
+// Inicializa o Firebase apenas uma vez
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
