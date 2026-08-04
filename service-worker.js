d154b4bb811f4295e34c27ba8704701e65e5959e
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-08-04 18:24:25 -0300

    Atualização v.12

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'cfc-na-palma-v11-dva-20260804';
+const CACHE_NAME = 'cfc-na-palma-v12-introducao-20260804';
 const STATIC_FILES = [
   './manifest.json', './auditoria.png', './pericia_contabil.png',
   './contabilidade_publica.png', './contabilidade_de_custos.png',
