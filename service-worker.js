7e2e48471b39b4ba1e059e8cefe2f2f151db4458
professormoreiracontabilidade-rgb <professormoreira.contabilidade@gmail.com>
2026-08-12 08:13:11 -0300

    Atualização v.20

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'cfc-na-palma-v19-pl-quizzes-20260809';
+const CACHE_NAME = 'cfc-na-palma-v20-cpc22-20260812';
 const STATIC_FILES = ['./manifest.json','./auditoria.png','./pericia_contabil.png','./contabilidade_publica.png','./contabilidade_de_custos.png','./etica_contabil.png','./contabilidade_geral.png','./direito.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
